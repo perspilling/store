fa61289fda3c1f83bb8d5bb82ade036a4e2e1296
perspilling <per@kodemaker.no>
2011-10-09 13:01:01 +0200

    First version of selectable comment dialog

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -7,6 +7,20 @@ $(document).ready(function() {
         modal: true,
         buttons: {
             "Legg til valgt kommentar": function() {
+                var checkedBoxes = $('[name="comment-checkbox-group"]:checked');
+                var commentLabels = $('label');
+                var cbNames = [];
+                var selectedComments = [];
+                if (checkedBoxes.length == 0) {
+                    selectedComments[i] = '<tr><td>Ingen kommentar</td></tr>';
+                } else {
+                    for (i = 0; i < checkedBoxes.length; i++) {
+                        cbNames[i] = checkedBoxes[i].id;
+                        selectedComments[i] = '<tr><td>' + commentLabels[i].innerText + '</td></tr>';
+                    }
+                }
+                $('#result').html('The following checkboxes are selected: <tt>' + cbNames.toString() + '</tt>');
+                $('#selected-comments tbody').html(selectedComments.toString());
                 $(this).dialog("close");
             },
             Cancel: function() {
